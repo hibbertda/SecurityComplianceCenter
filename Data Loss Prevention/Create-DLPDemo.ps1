25b504cfc56e5641011a5aaaeb56a71794e5c404
xamlok <hibbertda@outlook.com>
2017-11-03 16:33:36 -0400

    Added README

--- Data Loss Prevention/Create-DLPDemo.ps1
+++ Data Loss Prevention/Create-DLPDemo.ps1
@@ -1,44 +1,54 @@
 <#
+  .NOTES
+
+
   .SYNOPSIS 
-    Sets up DLP Demo
+    Create data types and policy to demo Office 365 DLP capabilities.
 
   .DESCRIPTION
     This script will create new sensitive information type and DLP policy
     in the Office 365 Security & Compliance Center for the purposes of doing
     a Demo of DLP features in Office 365.  
 
+    The script will utilize the native Security & Compliance Center PowerShell connection. 
+    This method does not support accounts that rely on modern authentication (MFA/Conditional 
+    Access), only basic authentication is supported. 
+
   .LINK
     Online version: https://github.com/hibbertda/SecurityComplianceCenter
 #>
-
-## Connect to S&C Center ##
 
 # Disclamer on authentication requirements for S&CC
 $text = ("As of [May 6th 2017] the Office 365 Security & Compliance Center doesn't support modern authentication.`n `
 You will need to use an account that is capable using Basic Auth, and that does not have MFA enabled. If MFA is enabled `
 the account will be unable to authenticate.")
-$a = new-object -comobject wscript.shell
-$b = $a.popup("$text",0,"DLP Demo Setup",0+48)	
+$window = new-object -comobject wscript.shell
+$pop = $window.popup("$text",0,"DLP Demo Setup",0+48)	
 
 # Attempt to connect to S&CC
 clear-host
-Write-host -ForegroundColor Yellow "Connecting to Office 365 - Security & Compliance Center`n"
-try{
-    $UserCredential = Get-Credential
-    $Session = New-PSSession -ConfigurationName Microsoft.Exchange `
-        -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ `
-        -Credential $UserCredential `
-        -Authentication Basic `
-        -AllowRedirection `
-        -ErrorAction stop
-    Import-PSSession $Session -ErrorAction stop
-}
-# If the connection fails exit out of the script
-catch{
-    Write-host -ForegroundColor red "Unable to connect to the Office 365 Security & Compliance Center"
-    Exit;
-}
 
+$ActiveSessions = Get-PSSession | Where-Object {$_.Availability -eq $true -and $_.ComputerName -like "*compliance.protection.outlook.com"}
+
+#if ($ActiveSessions.count -eq 0){
+  Write-host -ForegroundColor Yellow "Connecting to Office 365 - Security & Compliance Center`n"
+  try{
+      $UserCredential = Get-Credential
+      $Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+          -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ `
+          -Credential $UserCredential `
+          -Authentication Basic `
+          -AllowRedirection `
+          #-ErrorAction stop
+      Import-PSSession $Session #-ErrorAction stop
+  }
+  # If the connection fails exit out of the script
+  catch{
+      Write-host -ForegroundColor red "Unable to connect to the Office 365 Security & Compliance Center"
+      Exit
+  }
+#}
+#region sensitive_data_type
 # Demo sensitive data type XML
 [XML]$DemoDataType_XML = @"
 <?xml version="1.0" encoding="utf-8"?>
@@ -93,14 +103,16 @@ Write-Host -ForegroundColor yellow "Creating custom sensitive data type"
 Try{
   New-DlpSensitiveInformationTypeRulePackage `
   -FileData (Get-Content -Path $XMLExportPath -Encoding Byte) `
+  -FileData $DemoDataType_XML `
   -ErrorAction stop
 }
 catch {
   Write-host -ForegroundColor red "Unable to add data type"
   Exit
 }
+#endregion
 
-# Create Demo DLP Policy 
+#region Create_Demo_DLP_Policy 
 clear-host
 try {
   $PolicyName = "DEMO DLP Policy"
@@ -139,11 +151,12 @@ try {
       -ErrorAction stop
 }
 Catch {
-
-  Write-Host -ForegroundColor red "Unable to create DLP policy"; Exit
-
+  Write-Host -ForegroundColor red "Unable to create DLP policy"
+  Exit
 }
-## Clean up
+#endregion 
+
+#region Clean_up
 Clear-Host
 Write-Host -ForegroundColor green "DLP DEMO Setup Succesful!!!`n"
 Write-Host "Create documents / emails with a string starting with 'Find Me' and 9 random numbers to fire the policy`n"
@@ -152,3 +165,7 @@ Write-host -ForegroundColor yellow "`tFind Me 123456789`n"
 
 # Remove DLP XML
 Remove-Item -Path $XMLExportPath | Out-Null
+
+# Close session
+Get-PSSession $Session | Remove-PSSession
+#endregion
